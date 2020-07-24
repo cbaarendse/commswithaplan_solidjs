0a4fd5329b2b66eb9c5fc61758516526efe3b6a4
Constantijn Baarendse <cbaarendse@live.nl>
2020-07-24 18:57:45 +0200

    content structure

--- components/Home.svelte
+++ components/Home.svelte
@@ -12,6 +12,7 @@
     background-attachment: fixed;
     background-size: cover;
     width: auto;
+    min-height: 900px;
   }
   section.commsWithAPlan {
     background-image: url("City 920x570.png");
@@ -25,24 +26,51 @@
   section.contact {
     background-image: url("Payphone old 920x570.png");
   }
+  h1 {
+    background-color: yellow;
+  }
+  p {
+    background-color: white;
+  }
 </style>
 
 <section class="commsWithAPlan">
-  <h1>Comms With A Plan</h1>
-  {#if $language == 'english'}
-    <p>
-      Hello Comms With A Plan is a consultancy for advertisers. We initiate and
-      evaluate your marketing communications strategy, we manage your agencies
-      and your budget.
-    </p>
-  {/if}
+  <article>
+    <h1>Comms With A Plan</h1>
+    {#if $language == 'english'}
+      <p>
+        Hello Comms With A Plan is a consultancy for advertisers. We initiate
+        and evaluate your marketing communications strategy, we manage your
+        agencies and your budget.
+      </p>
+    {/if}
+    <p>Hello Comms With A Plan</p>
+  </article>
 </section>
 <section class="consultancy">
   <h1>Consultancy</h1>
+  <article>
+    <h2>Consultancy</h2>
+    <p>Hello Comms With A Plan</p>
+    <p>Hello Comms With A Plan</p>
+  </article>
+  <article>
+    <h2>Consultancy</h2>
+    <p>Hello Comms With A Plan</p>
+    <p>Hello Comms With A Plan</p>
+  </article>
 </section>
 <section class="about">
-  <h1>About</h1>
+  <article>
+    <h1>About</h1>
+    <p>Hello Comms With A Plan</p>
+    <p>Hello Comms With A Plan</p>
+  </article>
 </section>
 <section class="contact">
-  <h1>Contact</h1>
+  <article>
+    <h1>Contact</h1>
+    <p>Hello Comms With A Plan</p>
+    <p>Hello Comms With A Plan</p>
+  </article>
 </section>
