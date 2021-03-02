6242776c17890409129172299fa243147dd680ce
Constantijn Baarendse <cbaarendse@live.nl>
2021-03-02 11:08:56 +0100

    main and sub navigation;

--- imports/ui/components/NavigationMain.svelte
+++ imports/ui/components/NavigationMain.svelte
@@ -14,22 +14,24 @@
   export let path;
   export let language;
   let uiProvider = new UiProvider(language);
-
-  // functions
 </script>
 
-<nav>
+<nav class="main-nav">
   <ul>
     <li class="logo">
       <a href={'/'} class:active={path === '/'}>
         <LogoCommsWithAPlan size={'3.5rem'} colored={path === '/'} />
       </a>
+      <nav class="sub-nav" />
     </li>
 
     <li class="brand">
       <a href={'/commswithaplan'} class:active={path === '/commswithaplan'}>
-        <span class="blue">Comms</span>&nbsp;<span class="green">With&nbsp;A</span>&nbsp;<span class="red">Plan</span>
+        <span class="brand blue">Comms</span>&nbsp;<span class="brand green">With&nbsp;A</span>&nbsp;<span
+          class="brand red">Plan</span
+        >
       </a>
+      <nav class="sub-nav" />
     </li>
     <li>
       <a
@@ -41,29 +43,31 @@
       >
         {uiProvider.capitalizeAndSplit('reach')}
       </a>
-      <ul>
-        <li class="logo">
-          <a href={'/reach/'} class:active={path === '/reach/'}>
-            <LogoReachApp size={'2rem'} colored={path === '/reach/'} />
-          </a>
-        </li>
+      <nav class="sub-nav">
+        <ul>
+          <li>
+            <a href={'/reach/'} class:active={path === '/reach/'}>
+              <LogoReachApp size={'2rem'} colored={path === '/reach/'} />
+            </a>
+          </li>
 
-        <li>
-          <a href={'/reach/reachapp'} class:active={path === '/reach/reachapp'}>
-            <span class="brand">ReachApp</span>
-          </a>
-        </li>
-        <li>
-          <a href={'/reach/download'} class:active={path === '/reach/download'}>
-            {uiProvider.capitalizeAndSplit('download')}
-          </a>
-        </li>
-        <li>
-          <a href={'/reach/manual'} class:active={path === '/reach/manual'}>
-            {uiProvider.capitalizeAndSplit('manual')}
-          </a>
-        </li>
-      </ul>
+          <li>
+            <a href={'/reach/reachapp'} class:active={path === '/reach/reachapp'}>
+              <span class="brand">ReachApp</span>
+            </a>
+          </li>
+          <li>
+            <a href={'/reach/download'} class:active={path === '/reach/download'}>
+              {uiProvider.capitalizeAndSplit('download')}
+            </a>
+          </li>
+          <li>
+            <a href={'/reach/manual'} class:active={path === '/reach/manual'}>
+              {uiProvider.capitalizeAndSplit('manual')}
+            </a>
+          </li>
+        </ul>
+      </nav>
     </li>
   </ul>
 
@@ -73,34 +77,50 @@
   </form>
 </nav>
 
-<!-- Direction of the mouse from previous mouseover to current mouseleave determines the class -->
 <style>
   nav {
-    display: grid;
-    justify-content: space-between;
+    display: flex;
     margin: 0;
     padding: 0 1em;
-    background-color: var(--ra-teal-transparant);
-    height: var(--ra-nav-main-height);
     width: 100%;
     font-size: 1.4rem;
     z-index: 999;
   }
-
+  nav.main-nav {
+    justify-content: space-between;
+    background-color: var(--ra-teal-transparant);
+    height: var(--ra-nav-main-height);
+  }
+  nav.sub-nav {
+    justify-content: center;
+    background-color: var(--ra-teal-light-transparant);
+    height: var(--ra-nav-sub-height);
+  }
+  nav ul {
+    display: flex;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+  nav ul:first-child a.active {
+    background-color: transparent;
+    color: var(--ra-blue);
+  }
   nav li {
     display: flex;
     justify-content: center;
     align-items: center;
-    padding: 0 1em;
-    text-decoration: none;
   }
   @media only screen and (max-width: 768px) {
-    nav a {
-      display: none;
+    nav ul {
+      flex-direction: column;
     }
   }
+  li a {
+    text-decoration: none;
+  }
 
-  nav a.brand {
+  nav a span.brand {
     font-family: 'Trebuchet MS';
   }
   nav a:link {
@@ -121,10 +141,11 @@
     color: var(--ra-blue);
   }
 
-  nav a.logo.active {
+  nav a.active {
     background-color: transparent;
     color: var(--ra-blue);
   }
+
   nav a.active span.blue {
     color: var(--ra-blue);
   }
