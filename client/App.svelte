c5f64f60bacbbf075c37d40cbec9a3d52909e216
Constantijn Baarendse <cbaarendse@live.nl>
2021-09-07 18:29:26 +0200

    organized css navigation;

--- client/App.svelte
+++ client/App.svelte
@@ -38,21 +38,19 @@
   </label>
 
   <nav>
-    <div class="nav-1-wrapper">
-      <a
-        href={'javascript:void(0)'}
-        class:active={language === 'english'}
-        on:click={() => (language = 'english')}
-        tinro-ignore>Engels</a
-      >
-      <span>|</span>
-      <a
-        href={'javascript:void(0)'}
-        class:active={language === 'dutch'}
-        on:click={() => (language = 'dutch')}
-        tinro-ignore>Nederlands</a
-      >
-    </div>
+    <a
+      href={'javascript:void(0)'}
+      class:active={language === 'english'}
+      on:click={() => (language = 'english')}
+      tinro-ignore>Engels</a
+    >
+    <span>|</span>
+    <a
+      href={'javascript:void(0)'}
+      class:active={language === 'dutch'}
+      on:click={() => (language = 'dutch')}
+      tinro-ignore>Nederlands</a
+    >
     <Fa icon={faUser} size="0.6x" />
   </nav>
 
@@ -121,6 +119,8 @@
     position: absolute;
     display: grid;
     grid-template-columns: auto 1fr;
+    gap: 1em;
+    padding: 1em;
     width: 100%;
     z-index: 999;
   }
@@ -142,35 +142,22 @@
   }
 
   input ~ label > div.expand {
-    display: inline-block;
+    display: block;
   }
 
   input ~ label > div.collapse {
     display: none;
   }
 
-  input:checked ~ label {
-    transform: rotate(-45deg);
-    transition: transform 400ms ease-in-out 0ms;
-  }
-
-  input:checked ~ label > div.expand {
-    display: none;
-  }
-
-  input:checked ~ label > div.collapse {
-    display: inline-block;
-  }
-
   nav {
-    display: none;
-    margin: 0;
     width: 100%;
     font-family: 'Trebuchet MS';
     font-size: 1.4rem;
   }
 
   nav:nth-of-type(1) {
+    grid-column: 2/7;
+    display: grid;
     background-color: var(--ra-grey-off-white);
     transform: scale(1, 0);
     transform-origin: top;
@@ -178,6 +165,8 @@
   }
 
   nav:nth-of-type(2) {
+    grid-column: 1/3;
+    display: grid;
     background-color: var(--ra-teal);
     transform: scale(1, 0);
     transform-origin: top;
@@ -185,71 +174,15 @@
   }
 
   nav:nth-of-type(3) {
+    grid-column: 1/3;
+    display: grid;
     background-color: var(--ra-teal-light);
     transform: scale(1, 0);
     transform-origin: top;
     transition: transform 150ms ease-out 0ms;
   }
-  input:checked ~ nav:nth-of-type(1) {
-    transform: scale(1, 1);
-    transform-origin: top;
-    transition: transform 150ms ease-out 0ms;
-  }
-  input:checked ~ nav:nth-of-type(2) {
-    transform: scale(1, 1);
-    transform-origin: top;
-    transition: transform 150ms linear 150ms;
-  }
-  input:checked ~ nav:nth-of-type(3) {
-    transform: scale(1, 1);
-    transform-origin: top;
-    transition: transform 150ms ease-in 300ms;
-  }
 
-  @media screen and (min-width: 476px) {
-    input,
-    label {
-      display: none;
-    }
-    nav {
-      display: grid;
-      grid-column: auto;
-    }
-    nav:nth-of-type(1) {
-      grid-column: 2/3;
-      display: grid;
-      grid-template-columns: 1fr auto auto;
-      grid-template-rows: auto;
-      justify-items: end;
-      align-items: center;
-      transform: none;
-    }
-    nav:nth-of-type(2) {
-      grid-column: 1/3;
-      display: grid;
-      grid-template-columns: auto;
-      grid-template-rows: auto;
-      justify-items: start;
-      align-items: center;
-      row-gap: 0.3em;
-      transform: none;
-    }
-    nav:nth-of-type(3) {
-      grid-column: 1/3;
-      display: grid;
-      grid-template-columns: auto;
-      grid-template-rows: auto;
-      justify-items: start;
-      align-items: center;
-      row-gap: 0.3em;
-      transform: none;
-    }
-    header {
-      position: relative;
-    }
-  }
-
-  nav a {
+  header nav a {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -258,62 +191,62 @@
     text-decoration: none;
   }
 
+  header nav:nth-of-type(2) > a:nth-of-type(1) {
+    display: none;
+  }
+
   /* :link, :visited */
-  nav > a:link,
-  nav > a:visited {
+  header nav > a:link,
+  header nav > a:visited {
     color: var(--ra-white);
   }
 
   /* :hover */
-  nav:nth-of-type(1) > a:hover {
+  header nav:nth-of-type(1) > a:hover {
     color: var(--ra-grey-light);
   }
 
   /* :active */
-  nav a.active {
+  header nav a.active {
     color: var(--ra-blue);
   }
 
-  nav:nth-of-type(2) > a:hover span.blue,
-  nav:nth-of-type(2) > a:hover span.brand,
-  nav:nth-of-type(2) > a:active span.blue,
-  nav:nth-of-type(2) > a.active span.brand {
-    color: var(--ra-blue);
-  }
-  nav:nth-of-type(2) > a:hover span.green,
-  nav:nth-of-type(2) > a.active span.green {
-    color: var(--ra-green);
-  }
-  nav:nth-of-type(2) > a:hover span.red,
-  nav:nth-of-type(2) > a.active span.red {
-    color: var(--ra-red);
-  }
-
-  nav:nth-of-type(3) > a:hover span.brand {
-    color: var(--ra-blue);
-  }
-  div.nav-1-wrapper {
-    align-self: center;
-    display: flex;
-    align-items: center;
-  }
-
-  div.nav-1-wrapper > a,
-  div.nav-1-wrapper > a:visited {
+  header nav:nth-of-type(1) > a,
+  header nav:nth-of-type(1) > a:visited {
     font-size: 0.8rem;
     color: var(--ra-grey-light);
   }
 
-  div.nav-1-wrapper > a:hover {
+  header nav:nth-of-type(1) > a:hover {
     color: var(--ra-blue-bright);
   }
 
-  div.nav-1-wrapper > a.active {
+  header nav:nth-of-type(1) > a:active {
     background-color: transparent;
   }
-  div.nav-1-wrapper > span {
+  header nav:nth-of-type(1) > span {
     font-size: 0.8rem;
     color: var(--ra-grey-light);
+  }
+  header nav:nth-of-type(2) > a:hover span.blue,
+  header nav:nth-of-type(2) > a:hover span.brand,
+  header nav:nth-of-type(2) > a:active span.blue,
+  header nav:nth-of-type(2) > a.active span.brand {
+    color: var(--ra-blue);
+  }
+
+  header nav:nth-of-type(2) > a:hover span.green,
+  header nav:nth-of-type(2) > a.active span.green {
+    color: var(--ra-green);
+  }
+
+  header nav:nth-of-type(2) > a:hover span.red,
+  header nav:nth-of-type(2) > a.active span.red {
+    color: var(--ra-red);
+  }
+
+  header nav:nth-of-type(3) > a:hover span.brand {
+    color: var(--ra-blue);
   }
 
   main {
@@ -329,4 +262,81 @@
     background-color: var(--ra-white);
     border-top: 1px solid rgba(0, 0, 0, 0.1);
   }
+
+  @media screen and (min-width: 476px) {
+    header {
+      position: relative;
+    }
+    input {
+      all: unset;
+      display: none;
+      grid-column: 1/2;
+    }
+    label {
+      display: none;
+    }
+    nav {
+      display: grid;
+    }
+    nav:nth-of-type(1) {
+      display: grid;
+      grid-template-columns: 1fr auto auto auto;
+      grid-template-rows: auto;
+      justify-items: end;
+      align-items: center;
+      transform: none;
+    }
+    nav:nth-of-type(2) {
+      display: grid;
+      grid-template-columns: auto auto auto;
+      grid-template-rows: auto;
+      justify-items: start;
+      align-items: center;
+      row-gap: 0.3em;
+      transform: none;
+    }
+    nav:nth-of-type(3) {
+      display: grid;
+      grid-template-columns: auto auto auto;
+      grid-template-rows: auto;
+      justify-items: start;
+      align-items: center;
+      row-gap: 0.3em;
+      transform: none;
+    }
+    nav:nth-of-type(2) a:nth-of-type(1) {
+      display: grid;
+    }
+  }
+
+  /* 
+  input:checked ~ label {
+    transform: rotate(-45deg);
+    transition: transform 400ms ease-in-out 0ms;
+  }
+  input:checked ~ label > div.expand {
+    display: none;
+  }
+  input:checked ~ label > div.collapse {
+    display: block;
+  }
+  input:checked ~ nav:nth-of-type(1) {
+    display: grid;
+    transform: scale(1, 1);
+    transform-origin: top;
+    transition: transform 150ms ease-out 0ms;
+  }
+  input:checked ~ nav:nth-of-type(2) {
+    display: grid;
+    transform: scale(1, 1);
+    transform-origin: top;
+    transition: transform 150ms linear 150ms;
+  }
+
+  input:checked ~ nav:nth-of-type(3) {
+    display: grid;
+    transform: scale(1, 1);
+    transform-origin: top;
+    transition: transform 150ms ease-in 300ms;
+  } */
 </style>
