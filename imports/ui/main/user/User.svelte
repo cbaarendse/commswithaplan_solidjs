b322126f2031ce56edf6d7aec838edb92be29a8a
Constantijn Baarendse <cbaarendse@live.nl>
2023-01-14 13:17:48 +0100

    user component; fixtures;

---
+++ imports/ui/main/user/User.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<section>
+  <table>
+    <caption>Constantijn Baarendse</caption>
+    <thead>
+      <td />
+      <td />
+    </thead>
+  </table>
+  <thead>
+    <td />
+    <td />
+  </thead>
+</section>
+
+<style>
+</style>
--- server/fixtures.ts
+++ server/fixtures.ts
@@ -4,20 +4,21 @@ import {Accounts} from 'meteor/accounts-base';
 import {Roles} from 'meteor/alanning:roles';
 
 // Just for developing one user is being given the role of admin
-Meteor.startup(() => {
-  console.log('started up!!');
 
-  const harry = Meteor.users.findOne({username: 'harry'});
-  console.log('harry', harry);
+console.log('started up!!');
+console.log('Meteor.users find', Meteor.users.find({}).fetch());
+
+const harry = Meteor.users.findOne({username: 'harry'});
+console.log('harry', harry);
+
+if (harry) {
+  Roles.addUsersToRoles(harry._id, 'admin', Roles.GLOBAL_GROUP);
+}
+process.env.MAIL_URL = 'smtp://cbaarendse@gmail.com:284333@smtp.gmail.com:587';
+
+Accounts.onCreateUser((options, user) => {
+  // We're enforcing at least an empty profile object to avoid needing to check
+  // for its existence later.
+  user.profile = options.profile ? options.profile : {};
+  return user;
 });
-// if (harry) {
-//   Roles.addUsersToRoles(harry._id, 'admin', Roles.GLOBAL_GROUP);
-// }
-// process.env.MAIL_URL = 'smtp://cbaarendse@gmail.com:284333@smtp.gmail.com:587';
-
-// Accounts.onCreateUser((options, user) => {
-//   // We're enforcing at least an empty profile object to avoid needing to check
-//   // for its existence later.
-//   user.profile = options.profile ? options.profile : {};
-//   return user;
-// });
